9d6f41b12250edcaeb40ba99cbcec5cf45c9b33c
Anna Debenham <anna@maban.co.uk>
2024-01-14 16:33:01 +0000

    feat: add views

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node-sandbox
  Just my own sandbox for playing with Node.
+
+## Setup
+
+Run `npm run dev` in the terminal.
--- app.js
+++ app.js
@@ -1,29 +1,51 @@
+const dotenv = require('dotenv')
+const handlebars = require('express-handlebars');
 const express = require('express')
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
+const cors = require('cors');
 const app = express()
+
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+// Views
+
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.use(express.static('public'))
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+    defaultLayout: 'default',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials/'
+}));
 
 
 // Connections
 
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    console.log('✔ Connected to database')
-    app.listen(3000, ()=> {
-        console.log(`✔ Running on port 3000`)
+mongoose.connect('mongodb+srv://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD + process.env.MONGODB_URI).then(() => {
+    console.log('👍 Connected to database')
+    app.listen(process.env.PORT, ()=> {
+        console.log(`👍 Running on port ` + process.env.PORT)
     })    
 }).catch(() => {
-    console.log(error)
+    console.log('💩 Database connection failed')
 })
 
 
 // Routes
 
 app.get('/', (req, res) => {
-    res.send('Home')
+    res.render('home');
 })
 
 app.get('/account', (req, res) => {
-    res.send('Account')
+    res.render('account');
 })

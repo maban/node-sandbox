79c3d1b29da8c56c293bd559822df159c0ee3dfa
Anna Debenham <anna@maban.co.uk>
2024-01-14 18:47:08 +0000

    feat: add authentication

--- app.js
+++ app.js
@@ -1,51 +1,130 @@
-const dotenv = require('dotenv')
-const handlebars = require('express-handlebars');
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors');
-const app = express()
+// External modules
 
-// Middleware
+const dotenv = require("dotenv");
+const handlebars = require("express-handlebars");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+const port = process.env.PORT || "8000";
+
+require("dotenv").config();
+
+const authRouter = require("./auth");
+
+// Session Configuration
 
 app.use(cors());
 app.use(express.json());
 
+const session = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+};
 
-// Views
+if (app.get("env") === "production") {
+  session.cookie.secure = true;
+}
 
-app.set('view engine', 'hbs');
-app.set('views', './views');
+// Passport Configuration
 
-app.use(express.static('public'))
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL: process.env.AUTH0_CALLBACK_URL,
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    return done(null, profile);
+  }
+);
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-    defaultLayout: 'default',
-    layoutsDir: __dirname + '/views/layouts',
-    partialsDir: __dirname + '/views/partials/'
-}));
+// App configuration
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+    defaultLayout: "default",
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials/",
+  })
+);
 
 // Connections
 
-require('dotenv').config()
+mongoose
+  .connect(
+    "mongodb+srv://" +
+      process.env.MONGODB_USERNAME +
+      ":" +
+      process.env.MONGODB_PASSWORD +
+      process.env.MONGODB_URI
+  )
+  .then(() => {
+    console.log("👍 Connected to database");
+    app.listen(port, () => {
+      console.log(`👍 Running on port ` + port);
+    });
+  })
+  .catch(() => {
+    console.log("💩 Database connection failed");
+  });
 
-mongoose.connect('mongodb+srv://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD + process.env.MONGODB_URI).then(() => {
-    console.log('👍 Connected to database')
-    app.listen(process.env.PORT, ()=> {
-        console.log(`👍 Running on port ` + process.env.PORT)
-    })    
-}).catch(() => {
-    console.log('💩 Database connection failed')
-})
+// Router mounting
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
 
-// Routes
+app.use("/", authRouter);
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
+// Public routes
 
-app.get('/account', (req, res) => {
-    res.render('account');
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// Secured routes
+
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+};
+
+app.get("/account", secured, (req, res, next) => {
+  const { _raw, _json, ...userProfile } = req.user;
+  res.render("account", {
+    title: "Account",
+    userProfile: userProfile,
+  });
+});
